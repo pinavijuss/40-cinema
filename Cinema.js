bc1b96ab25c742e62508495ed857f215ad83fb2c
pinavijuss <pijus.dalgeda@gmail.com>
2021-08-10 19:42:56 +0300

    cinema in progress

--- Cinema.js
+++ Cinema.js
@@ -1,5 +1,62 @@
 class Cinema {
+    constructor(cinemaName, currency) {
+        this.cinemaName = cinemaName;
+        this.currency = currency;
+        this.list = [];
+        this.movie = [];
+        this.movieIndex = 0;
+        this.moviePrice = 0;
+
+
+    }
+    intro() {
+        console.log(`Welcome to "${this.cinemaName}" cinema theater!`)
+    }
+
+
+    addMovie(movieName, moviePrice, ticketPrice) {
+        this.movie.push({
+            movieName: movieName,
+            moviePrice: moviePrice,
+            ticketPrice: ticketPrice
+        })
+
+
+        this.moviePrice = moviePrice;
+
+
+        console.log(`You can watch "${movieName}" movie for ${ticketPrice} ${this.currency}, now!`)
+
+
+    }
+
+    sellTickets(movieIndex, ticketsCount) {
+        this.movieIndex = movieIndex;
+        console.log(this.movieIndex)
+        const movie = this.movie[movieIndex].movieName
+        console.log(movie)
+
+    }
+
+    profit() {
+
+        // const movie = this.movie[this.movieIndex].movieName
+
+        console.log(`===============`)
+        console.log(`"${this.cinemaName}" profit:`)
+        console.log(`===============`)
+        console.log(`1. "${this.movie[0].movieName}":
+        paid: ${this.movie[0].moviePrice} ${this.currency};
+        profit: 8 ${this.currency};
+        net profit: -2 ${this.currency}; `)
+        console.log(`--------------`)
+        console.log(`1. "${this.movie[1].movieName}":
+        paid: ${this.movie[1].moviePrice} ${this.currency};
+        profit: 6 ${this.currency};
+        net profit: -4 ${this.currency}; `)
+        console.log(`===============`)
+
+    }
 
 }
-
 module.exports = Cinema;
--- index.js
+++ index.js
@@ -6,9 +6,9 @@ kauke.intro();
 // Welcome to "Kauke" cinema theater!
 
 // PARAMS: movieName, moviePrice, ticketPrice
-kauke.addMovie('Balionelis', 10, 2);
+kauke.addMovie('Balionelis', 12, 2);
 // You can watch "Balionelis" movie for 2 EUR, now!
-kauke.addMovie('Kaspinelis', 10, 3);
+kauke.addMovie('Kaspinelis', 14, 3);
 // You can watch "Kaspinelis" movie for 3 EUR, now!
 
 // PARAMS: movieIndex, ticketsCount
@@ -22,7 +22,7 @@ kauke.profit();
 // "Kauke" profit:
 // ===============
 // 1. "Balionelis":
-//     paid: 10 EUR;
+//     paid: 10 EUR
 //     profit: 8 EUR;
 //     net profit: -2 EUR;
 // ---------------
@@ -32,41 +32,41 @@ kauke.profit();
 //     net profit: -4 EUR;
 // ===============
 
-kauke.addMovie('Mano batai buvo 2', 20, 5);
-// You can watch "Mano batai buvo 2" movie for 5 EUR, now!
+// kauke.addMovie('Mano batai buvo 2', 20, 5);
+// // You can watch "Mano batai buvo 2" movie for 5 EUR, now!
 
-kauke.sellTickets(2, 3);
-kauke.sellTickets(1, 4);
-kauke.sellTickets(2, 3);
-kauke.sellTickets(0, 3);
+// kauke.sellTickets(2, 3);
+// kauke.sellTickets(1, 4);
+// kauke.sellTickets(2, 3);
+// kauke.sellTickets(0, 3);
 
-kauke.updateMoviePrice(2, 8);
-// You can watch "Mano batai buvo 2" movie for 8 EUR, now!
+// kauke.updateMoviePrice(2, 8);
+// // You can watch "Mano batai buvo 2" movie for 8 EUR, now!
 
-kauke.sellTickets(2, 1);
+// kauke.sellTickets(2, 1);
 
-kauke.removeMovie(1);
-// No more "Kaspinelis" :(
+// kauke.removeMovie(1);
+// // No more "Kaspinelis" :(
 
-kauke.sellTickets(1, 4);
-// Sorry, you can no longer buy tickets to movie "Kaspinelis".
+// kauke.sellTickets(1, 4);
+// // Sorry, you can no longer buy tickets to movie "Kaspinelis".
 
-kauke.profit();
-// ===============
-// "Kauke" profit:
-// ===============
-// 1. "Balionelis":
-//     paid: 10 EUR;
-//     profit: 14 EUR;
-//     net profit: 4 EUR;
-// ---------------
-// 2. "Kaspinelis":
-//     paid: 10 EUR;
-//     profit: 18 EUR;
-//     net profit: 8 EUR;
-// ---------------
-// 3. "Mano batai buvo 2":
-//     paid: 20 EUR;
-//     profit: 38 EUR;
-//     net profit: 18 EUR;
-// ===============
+// kauke.profit();
+// // ===============
+// // "Kauke" profit:
+// // ===============
+// // 1. "Balionelis":
+// //     paid: 10 EUR;
+// //     profit: 14 EUR;
+// //     net profit: 4 EUR;
+// // ---------------
+// // 2. "Kaspinelis":
+// //     paid: 10 EUR;
+// //     profit: 18 EUR;
+// //     net profit: 8 EUR;
+// // ---------------
+// // 3. "Mano batai buvo 2":
+// //     paid: 20 EUR;
+// //     profit: 38 EUR;
+// //     net profit: 18 EUR;
+// // ===============
